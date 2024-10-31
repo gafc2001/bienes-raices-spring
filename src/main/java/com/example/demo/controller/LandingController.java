be1a2fb1289c60ece7df40bd2fa8e9b58c650797
Luis <lualtale@hotmail.com>
2024-10-31 16:28:18 -0500

    administrador

--- src/main/java/com/example/demo/controller/LandingController.java
+++ src/main/java/com/example/demo/controller/LandingController.java
@@ -4,10 +4,14 @@ import com.example.demo.Service.AnuncioService;
 import com.example.demo.Service.BlogService;
 import com.example.demo.model.Anuncio;
 import com.example.demo.model.Blog;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -43,5 +47,41 @@ public class LandingController {
     @GetMapping("contacto")
     public String contacto(){
         return "contacto";
+    }
+
+    @GetMapping("administrador")
+    public String administrador(){return "administrador";}
+
+    @PostMapping("administrador")
+    public String administrador(@RequestParam String username,
+                                @RequestParam String password,
+                                HttpServletRequest request,
+                                Model model){
+        if ("admin".equals(username) && "admin".equals(password)){
+            HttpSession session= request.getSession();
+            session.setAttribute("username",username);
+            return "redirect:/dashboard";
+        }else {
+            model.addAttribute("error",true);
+            return "administrador";
+        }
+    }
+    @GetMapping("/dashboard")
+    public String dashboard(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("username") != null) {
+            model.addAttribute("username", session.getAttribute("username"));
+            return "dashboard";
+        }
+        return "redirect:/administrador";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/administrador";
     }
 }
